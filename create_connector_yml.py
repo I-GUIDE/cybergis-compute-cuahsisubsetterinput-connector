8b787e836643c744179937eabb3cb899965f84ab
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2023-02-06 12:20:57 -0500

    Update create_connector_yml.py

--- create_connector_yml.py
+++ create_connector_yml.py
@@ -3,7 +3,10 @@
 import yaml
 import os
 
-connector_dict = {'$1': {'Input': {'CUAHSISubsetterInput': {'huc12_id':os.environ['param_huc12_id']}}}}
+huc12_ids_str = os.environ['param_huc12_id']
+huc12_ids_list = huc12_ids_str.split(',')
+
+connector_dict = {'$1': {'Input': {'CUAHSISubsetterInput': {'huc12_id':huc12_ids_list}}}}
 
 with open('/job/executable/cuahsisubsetterinput.yml','w') as subsetterfile:
     yaml.dump(connector_dict,subsetterfile)
